01cc5eaa2e8c353cfa406cd95bd7046294e7e547
jesusdario <jesusdriru@gmail.com>
2015-04-08 17:20:06 +0200

    removes unnecesary code

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
+// xyos apps must be executable
+// and carry this upper shebang! with them
+
 /* Requires node.js libraries */
 var http = require('http');
 var fs = require('fs');
-var argv = require('minimist')(process.argv.slice(2)); // must-have package
-var colors = require('colors'); // just to make a colourful prompt
+var argv = require('minimist')(process.argv.slice(2));
 
-/* xbian apps need to accept the port to be launched by parameters */
+// xyos apps can accept port through parameters like: --port <free port>
+// no configuration needed
 port = argv.port;
-
 if(isNaN(port)) {
-	error = "Port \"" + port + "\" is not a number.";
-	console.log(error.red);
+	console.log("Port \"%s\" is not a number.", port);
 	process.kill(1);
 } else {
-	success = "Listening on port " + port;
-	console.log(success.green);
+	console.log("Listening on port %s", port);
 }
 
 http.createServer(function(request, response) {  
-	/* index.html is an user interface example */
+	// index.html is an user interface example
+	// you can choose from any web app framework you want
+	// or even not having a user interface is fine!
 	fs.readFile('index.html', 'utf8', function (error, data) {
-		if (error) {
-			return console.log(error.red);
-		}
+		if (error)
+			return console.error(error);
 		response.writeHeader(200, {"Content-Type": "text/html"});  
 		response.write(data);  
 		response.end();  
 	});
-
 }).listen(port);
